367e41df132c8b4f113ec428318750aaece10f29
Golden senior <felipescarpes23@gmail.com>
2023-05-07 02:46:13 -0300

    adicionando modelo de gt

--- database/database.py
+++ database/database.py
@@ -1,31 +1,33 @@
 import sqlite3
+from models.gt import GT
+
+def insert_gt(gt: GT):
+    
+    conn.execute("INSERT INTO GTS (name,title, history) VALUES (?, ?, ?)", (name,title, history))
+    #passa para o bando de dados oq foi escrito
+    conn.commit()
 
 # Conectar ao banco de dados 
 conn = sqlite3.connect('exemplo.db')
 
-# Criar uma tabela gts
-conn.execute('''CREATE TABLE GTS
-             (nome TEXT NOT NULL,
-              titulo TEXT NOT NULL,
-              historia TEXT NOT NULL)''')
+try:
+    # Criar uma tabela gts
+    conn.execute('''CREATE TABLE IF NOT EXISTS GTS
+                (name TEXT NOT NULL,
+                title TEXT NOT NULL,
+                history TEXT NOT NULL)''')
 
-nome_usuario ="felipe"
-titulo="historia"
-texto_mensagem ="emocionante"
+    insert_gts("felipe", "oi","comovai")
+    # Printa tabela
+    print(conn.execute("""SELECT * FROM GTS""").fetchall())
+except Exception as err:
+    print(err)
+finally:    
+    # Deleta tabela
+    conn.execute('''DROP TABLE GTS''')
 
-#passa para o bando de dados oq foi escrito
-conn.execute("INSERT INTO GTS (nome,titulo, historia) VALUES (?, ?, ?)", (nome_usuario,titulo, texto_mensagem))
+    #salva as alterações no banco de dados
+    conn.commit()
 
-
-
-# Printa tabela
-print(conn.execute("""SELECT * FROM GTS""").fetchall())
-
-# Deleta tabela
-conn.execute('''DROP TABLE GTS''')
-
-#salva as alterações no banco de dados
-conn.commit()
-
-# Fechar a conexão com o banco de dados
-conn.close() 
+    # Fechar a conexão com o banco de dados
+    conn.close() 
